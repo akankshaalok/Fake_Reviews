b0342eb7b55ab9951a796901013aa524dbeba85c
Akanksha Alok <13605081+akankshaalok@users.noreply.github.com>
2018-08-01 21:02:15 -0400

    Add files via upload

---
+++ detector_SVM.py
@@ -0,0 +1,185 @@
+from sklearn import svm
+import random
+'''
+Used to read in X matrix (data set).
+'''
+with open("./spambase/spambase.data", 'r') as fobj:
+    X = [[float(num) for num in line.split(',')] for line in fobj]
+
+random.shuffle(X)
+
+'''
+Used to read in Y vector (attribute names).
+'''
+y = []
+for row in X:
+    y.append(row[len(row)-1])
+
+
+'''
+We're going to stratify X in containing only word freq, char freq, capital freq
+and compare the different accuracy rates and false positivies using these prediction
+models.
+'''
+#Obtaining only word attributes
+wordX = []
+for row in X:
+    wordX.append(row[:48])
+
+#Training calssifier on first 4000 samples
+clfWord = svm.SVC()
+clfWord.fit(wordX[0:4000], y[0:4000])
+testWordX = clfWord.predict(wordX[4000:])
+
+#Obtaining prediction accuracy and false positive rate
+countWord = 0
+countWordFalse = 0
+for i in range(len(testWordX)):
+    if testWordX[i] == y[4000+i]:
+        countWord = countWord + 1
+    if testWordX[i] == 1 and y[4000+i] == 0:
+        countWordFalse = countWordFalse + 1
+
+print("Words:")
+print("Percent spam = " + str(countWord/len(y[4000:])))
+print("Percent false positive = " + str(countWordFalse/len(y[4000:])))
+print()
+#-------------------------------------------------------
+#Obtaining only char attributes
+charX = []
+for row in X:
+    charX.append(row[48:54])
+
+#Training classifier on first 4000 samples
+clfChar = svm.SVC()
+clfChar.fit(charX[0:4000], y[0:4000])
+testCharX = clfChar.predict(charX[4000:])
+
+#Obtaining prediction accuracy and false positive rate
+countChar = 0
+countCharFalse = 0
+for i in range(len(testCharX)):
+    if testCharX[i] == y[4000+i]:
+        countChar = countChar + 1
+    if testCharX[i] == 1 and y[4000+i] == 0:
+        countCharFalse = countCharFalse + 1
+print("Chars:")
+print("Percent spam = " + str(countChar/len(y[4000:])))
+print("Percent false positive = " + str(countCharFalse/len(y[4000:])))
+print()
+
+#-----------------------------------------------------
+#Obtaining only capitalization attributes
+capX = []
+for row in X:
+    capX.append(row[54:])
+
+#Training classifier on first 4000 samples
+clfCap = svm.SVC()
+clfCap.fit(capX[0:4000], y[0:4000])
+testCapX = clfCap.predict(capX[4000:])
+
+#Obtaining prediction accuracy and false positive rate
+countCap = 0
+countCapFalse = 0
+for i in range(len(testCapX)):
+    if testCapX[i] == y[4000+i]:
+        countCap = countCap + 1
+    if testCapX[i] == 1 and y[4000+i] == 0:
+        countCapFalse = countCapFalse + 1
+print("Capital:")
+print("Percent spam = " + str(countCap/len(y[4000:])))
+print("Percent false positive = " + str(countCapFalse/len(y[4000:])))
+print()
+
+#-----------------------------------------------------
+#Obtaining word and char attributes
+wordCharX = []
+for row in X:
+    wordCharX.append(row[:54])
+
+#Training classifier on first 4000 samples
+clfWordChar = svm.SVC()
+clfWordChar.fit(wordCharX[0:4000], y[0:4000])
+testWordCharX = clfWordChar.predict(wordCharX[4000:])
+
+#Obtaining prediction accuracy and false positive rate
+countWordChar = 0
+countWordCharFalse = 0
+for i in range(len(testWordCharX)):
+    if testWordCharX[i] == y[4000+i]:
+        countWordChar = countWordChar + 1
+    if testWordCharX[i] == 1 and y[4000+i] == 0:
+        countWordCharFalse = countWordCharFalse + 1
+print("Words and characters:")
+print("Percent spam = " + str(countWordChar/len(y[4000:])))
+print("Percent false positive = " + str(countWordCharFalse/len(y[4000:])))
+print()
+
+#-----------------------------------------------------
+#Obtaining word and cap attributes
+wordCapX = []
+for row in X:
+    wordCapX.append(row[:48] + row[54:])
+
+#Training classifier on first 4000 samples
+clfWordCap = svm.SVC()
+clfWordCap.fit(wordCapX[0:4000], y[0:4000])
+testWordCapX = clfWordCap.predict(wordCapX[4000:])
+
+#Obtaining prediction accuracy and false positive rate
+countWordCap = 0
+countWordCapFalse = 0
+for i in range(len(testWordCapX)):
+    if testWordCapX[i] == y[4000+i]:
+        countWordCap = countWordCap + 1
+    if testWordCapX[i] == 1 and y[4000+i] == 0:
+        countWordCapFalse = countWordCapFalse + 1
+print("Words and capitals:")
+print("Percent spam = " + str(countWordCap/len(y[4000:])))
+print("Percent false positive = " + str(countWordCapFalse/len(y[4000:])))
+print()
+
+#-----------------------------------------------------
+#Obtaining char and cap attributes
+charCapX = []
+for row in X:
+    charCapX.append(row[48:])
+
+#Training classifier on first 4000 samples
+clfCharCap = svm.SVC()
+clfCharCap.fit(charCapX[0:4000], y[0:4000])
+testCharCapX = clfCharCap.predict(charCapX[4000:])
+
+#Obtaining prediction accuracy and false positive rate
+countCharCap = 0
+countCharCapFalse = 0
+for i in range(len(testCharCapX)):
+    if testCharCapX[i] == y[4000+i]:
+        countCharCap = countCharCap + 1
+    if testCharCapX[i] == 1 and y[4000+i] == 0:
+        countCharCapFalse = countCharCapFalse + 1
+print("Characters and capitals:")
+print("Percent spam = " + str(countCharCap/len(y[4000:])))
+print("Percent false positive = " + str(countCharCapFalse/len(y[4000:])))
+print()
+
+#-----------------------------------------------------
+#All attributes
+#Training classifier on first 4000 samples
+clf = svm.SVC()
+clf.fit(X[0:4000], y[0:4000])
+testX = clf.predict(X[4000:])
+
+#Obtaining prediction accuracy and false positive rate
+count = 0
+countFalse = 0
+for i in range(len(testX)):
+    if testX[i] == y[4000+i]:
+        count = count + 1
+    if testX[i] == 1 and y[4000+i] == 0:
+        countFalse = countFalse + 1
+print("Total:")
+print("Percent spam = " + str(count/len(y[4000:])))
+print("Percent false positive = " + str(countFalse/len(y[4000:])))
+print()
---
+++ generator.py
@@ -0,0 +1,246 @@
+import sys
+import collections
+import os
+import string
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import random
+import numpy as np
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+'''
+Read's each line and obtains the words, separated by spaces and
+places in dictionary (wordcount).
+Case and punctuation are distinct. (Should I remove?)
+Also how to stratify? All fake together? Separate by pos/neg? Probably test both.
+'''
+translator = str.maketrans('', '', string.punctuation)
+
+'''
+Combines all fake reviews and gets word counts
+
+ or word_tag[0][1] == "NNPS" or word_tag[0][1] == "RB" or word_tag[0][1] == "RBR" or word_tag[0][1] == "RBS" or word_tag[0][1] == "VB" or word_tag[0][1] == "VBD" or word_tag[0][1] == "VBG" or word_tag[0][1] == "VBN" or word_tag[0][1] == "VBZ"
+'''
+
+'''
+wordsFake = collections.Counter()
+wordsNegFake = collections.Counter()
+wordsPosFake = collections.Counter()
+
+wordsReal = collections.Counter()
+wordsPosReal = collections.Counter()
+wordsNegReal = collections.Counter()
+
+wordsFakeCount = 0
+wordsNegFakeCount = 0
+wordsPosFakeCount = 0
+
+wordsRealCount = 0
+wordsPosRealCount = 0
+wordsNegRealCount = 0
+
+numberOfFakeReviews = 0
+numberOfNegFakeReviews = 0
+numberOfPosFakeReviews = 0
+
+numberOfRealReviews = 0
+numberOfNegRealReviews = 0
+numberOfPosRealReviews = 0
+
+exclaimFake = 0
+exclaimNegFake = 0
+exclaimPosFake = 0
+
+exclaimReal = 0
+exclaimNegReal = 0
+exclaimPosReal = 0
+
+#Negative fake
+for i in range (1,6):
+    path = "op_spam_v1.4/op_spam_v1.4/negative_polarity/deceptive_from_MTurk/fold" + str(i) + "/"
+    for filename in os.listdir(path):
+        with open(path + filename,"r") as f:
+            #print(path + filename)
+            numberOfNegFakeReviews = numberOfNegFakeReviews + 1
+            numberOfFakeReviews = numberOfFakeReviews + 1
+            for line in f:
+                for word in line.split():
+                    wordsFakeCount = wordsFakeCount + 1
+                    wordsNegFakeCount = wordsNegFakeCount + 1
+                    word_token = nltk.word_tokenize(word)
+                    word_tag = nltk.pos_tag(word_token)
+                    if word_tag[0][1] == "JJ" or word_tag[0][1] == "JJR" or word_tag[0][1] == "JJS":
+                        wordsFake[word.translate(translator).lower()] += 1
+                        wordsNegFake[word.translate(translator).lower()] += 1
+                    if "!" in word:
+                        exclaimFake = exclaimFake + 1
+                        exclaimNegFake = exclaimNegFake + 1
+
+#Positive fake
+for i in range (1,6):
+    path = "op_spam_v1.4/op_spam_v1.4/positive_polarity/deceptive_from_MTurk/fold" + str(i) + "/"
+    for filename in os.listdir(path):
+        with open(path + filename,"r") as f:
+            #print(path + filename)
+            numberOfPosFakeReviews = numberOfPosFakeReviews + 1
+            numberOfFakeReviews = numberOfFakeReviews + 1
+            for line in f:
+                for word in line.split():
+                    wordsFakeCount = wordsFakeCount + 1
+                    wordsPosFakeCount = wordsPosFakeCount + 1
+                    word_token = nltk.word_tokenize(word)
+                    word_tag = nltk.pos_tag(word_token)
+                    if word_tag[0][1] == "JJ" or word_tag[0][1] == "JJR" or word_tag[0][1] == "JJS":
+                        wordsFake[word.translate(translator).lower()] += 1
+                        wordsPosFake[word.translate(translator).lower()] += 1
+                    if "!" in word:
+                        exclaimFake = exclaimFake + 1
+                        exclaimPosFake = exclaimPosFake + 1
+'''
+def stem(text):
+    text_stem = [stemmer.stem(token) for token in text.split(' ')]
+    text_stem_join = ' '.join(text_stem)
+    return text_stem_join
+
+reviews = []
+sentencePoolString = ""
+#Negative real
+for i in range (1,6):
+    path = "op_spam_v1.4/op_spam_v1.4/negative_polarity/truthful_from_Web/fold" + str(i) + "/"
+    for filename in os.listdir(path):
+        with open(path + filename,"r") as f:
+            review = ""
+            for line in f:
+                review = review + line.strip()
+                sentencePoolString = sentencePoolString + line.strip()
+
+            #reviewSent = review.replace("!", ".").split(".")
+            reviewSent = nltk.sent_tokenize(review)
+            reviews.append(reviewSent)
+
+baseReview = random.choice(reviews)
+baseReviewString = ' '.join(baseReview)
+#print(baseReviewToken)
+
+sentencePool = nltk.sent_tokenize(sentencePoolString)
+
+'''
+WHY WONT IT REMOVE THE SENTENCES FROM THE BASE REVIEW?!?!
+'''
+
+for sentence in baseReview:
+    if sentence in sentencePool:
+        sentencePool.remove(sentence)
+
+print(baseReviewString)
+
+newReview = ""
+
+index_list = []
+for sentence in baseReview:
+    tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+    tfidf_matrix = tfidf_vectorizer.fit_transform([sentence] + sentencePool)
+    #print(tfidf_matrix[0])
+    arr = cosine_similarity(tfidf_matrix[0], tfidf_matrix)
+    max = 0
+    index = 0
+    count = 0
+    #print(arr[0][0])
+    for item in arr[0]:
+        #print(count, item)
+        if item < .99 and item > max and count not in index_list:
+            #print(item)
+            max = item
+            index = count
+        count = count + 1
+    #print(index)
+    index_list.append(index)
+    print(sentencePool[index])
+    newReview = newReview + sentencePool[index] + " "
+print(index_list)
+
+print()
+print(newReview)
+
+
+'''
+#Positive real
+for i in range (1,6):
+    path = "op_spam_v1.4/op_spam_v1.4/positive_polarity/truthful_from_TripAdvisor/fold" + str(i) + "/"
+    for filename in os.listdir(path):
+        with open(path + filename,"r") as f:
+            #print(path + filename)
+            #print(f)
+            #pos_real = nltk.sent_tokenize(text)
+
+            numberOfPosRealReviews = numberOfPosRealReviews + 1
+            numberOfRealReviews = numberOfRealReviews + 1
+
+            for line in f:
+
+                for word in line.split():
+                    wordsPosRealCount = wordsPosRealCount + 1
+                    wordsRealCount = wordsRealCount + 1
+                    word_token = nltk.word_tokenize(word)
+                    word_tag = nltk.pos_tag(word_token)
+                    if word_tag[0][1] == "JJ" or word_tag[0][1] == "JJR" or word_tag[0][1] == "JJS":
+                        wordsReal[word.translate(translator).lower()] += 1
+                        wordsPosReal[word.translate(translator).lower()] += 1
+                    if "!" in word:
+                        exclaimReal = exclaimReal + 1
+                        exclaimPosReal = exclaimPosReal + 1
+
+
+print(pos_real)
+'''
+
+'''
+print("Most common words in Fake reviews:")
+print(collections.Counter(wordsFake).most_common(10))
+print("Average word count in Fake reviews:")
+print(wordsFakeCount/numberOfFakeReviews)
+print("Average exclaimation point in Fake reviews:")
+print(exclaimFake/numberOfFakeReviews)
+
+
+print("\n Most common words in Negative Fake Reviews")
+print(collections.Counter(wordsNegFake).most_common(10))
+print("Average word count in Negative Fake reviews:")
+print(wordsNegFakeCount/numberOfNegFakeReviews)
+print("Average exclaimation point in Negative Fake reviews:")
+print(exclaimNegFake/numberOfNegFakeReviews)
+
+
+print("\n Most common words in Positive Fake Reviews")
+print(collections.Counter(wordsPosFake).most_common(10))
+print("Average word count in Positive Fake reviews:")
+print(wordsPosFakeCount/numberOfPosFakeReviews)
+print("Average exclaimation point in Positive Fake reviews:")
+print(exclaimPosFake/numberOfPosFakeReviews)
+
+
+print("\n Most common words in Real Reviews")
+print(collections.Counter(wordsReal).most_common(10))
+print("Average word count in Real reviews:")
+print(wordsRealCount/numberOfRealReviews)
+print("Average exclaimation point in Real reviews:")
+print(exclaimReal/numberOfRealReviews)
+
+
+print("\n Most common words in Negtaive Real Reviews")
+print(collections.Counter(wordsNegReal).most_common(10))
+print("Average word count in Negative Real reviews:")
+print(wordsNegRealCount/numberOfNegRealReviews)
+print("Average exclaimation point in Negative Real reviews:")
+print(exclaimNegReal/numberOfNegRealReviews)
+
+
+print("\n Most common words in Positive Real Reviews")
+print(collections.Counter(wordsPosReal).most_common(10))
+print("Average word count in Positive Real reviews:")
+print(wordsPosRealCount/numberOfPosRealReviews)
+print("Average exclaimation point in Positive Real reviews:")
+print(exclaimPosReal/numberOfPosRealReviews)
+'''
+f.close()
